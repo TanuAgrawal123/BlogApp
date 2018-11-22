6b17a6382d63a84e3e15e7dcea0cb412cd3e78ac
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2018-11-22 14:27:06 +0530

    Removed issues

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bmfk-7&pkq%-*68z5i@(m#s8cue2@x7kjsn6nq5fz$zl6n+7ff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
